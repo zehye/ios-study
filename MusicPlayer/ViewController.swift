45793338ab5ec90f4fa2c447228e51e93273c565
tommy <tommy@emmental.co.kr>
2020-01-22 00:08:54 +0900

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Sumin Lee on 2020/01/22.
+//  Copyright © 2020 Sumin Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
